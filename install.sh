c9a06881bb6b4535654ecf166550b99482d61839
voin57rus <88772553+voin57rus@users.noreply.github.com>
2026-05-26 23:26:43 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,13 +6,10 @@ cd /root || exit 1
 
 rm -rf svaboda_admin
 
+echo "📥 Cloning private repo..."
 git clone git@github.com:voin57rus/svaboda_admin.git
 
 cd svaboda_admin || exit 1
 
-if [ -f "install.sh" ]; then
-    bash install.sh
-else
-    echo "❌ install.sh not found"
-    exit 1
-fi
+echo "⚙️ Running bot installer..."
+bash install.sh
